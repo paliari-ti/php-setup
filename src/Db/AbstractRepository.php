ad40198b5f870f2c29cd650d2a67dab83f0dd79a
Marcos Paliari <marcos@paliari.com.br>
2019-11-11 14:49:21 -0300

    refactored Db/AbstractRepository

--- src/Db/AbstractRepository.php
+++ src/Db/AbstractRepository.php
@@ -2,9 +2,46 @@
 
 namespace Paliari\PhpSetup\Db;
 
+use Doctrine\Common\Persistence\Mapping\MappingException;
+use Doctrine\DBAL\LockMode;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Paliari\Doctrine\ModelException;
 use Paliari\PhpSetup\Db\VO\PaginatedVO;
 use Paliari\Utils\AbstractSingleton;
 
+/**
+ * Class AbstractRepository
+ *
+ * @method AbstractModel|null find($id, bool $throw = false)
+ * @method AbstractModel|null findLock($id, int $lockMode = LockMode::PESSIMISTIC_WRITE, ?int $lockVersion = null, $throw = false)
+ * @method AbstractModel|null findOneBy(array $criteria)
+ * @method AbstractModel      findOrInitializeBy(array $params, array $filter_fields = [])
+ * @method array              findBy($criteria, $orderBy = null, $limit = null, $offset = null)
+ * @method bool               exists(array $ransack_params)
+ * @method QB                 ransack(array $params)
+ * @method QB                 ransackForUpdate(array $params)
+ * @method AbstractModel|null first()
+ * @method AbstractModel|null last()
+ * @method array              all()
+ * @method array              attributes()
+ * @method array              exportOnlyAttributes()
+ * @method array              hideAttributes()
+ * @method string             tableName()
+ * @method string             tableize()
+ * @method string             hum()
+ * @method string             humAttribute(string $attribute)
+ * @method bool               hasField(string $field)
+ * @method bool               typeOfField(string $field)
+ * @method bool               hasAssociation(string $property)
+ * @method array|null         getAssociationMapping(string $property)
+ * @method string|null        targetEntity(string $property)
+ * @method array              getAssociationMappings()
+ * @method int|null           getAssociationType(string $property)
+ * @method ClassMetadata      getClassMetadata()
+ * @method EM                 getEm()
+ *
+ * @package Paliari\PhpSetup\Db
+ */
 abstract class AbstractRepository extends AbstractSingleton implements RepositoryInterface
 {
     abstract protected function modelName(): string;
@@ -69,50 +106,14 @@ abstract class AbstractRepository extends AbstractSingleton implements Repositor
      * @param bool  $throw
      *
      * @return bool
+     * @throws MappingException
+     * @throws ModelException
      */
     public function delete($id, bool $throw = true): bool
     {
         $model = $this->find($id, $throw);
 
         return $model->destroy($throw);
-    }
-
-    public function find($id, bool $throw = false)
-    {
-        $model = $this->modelName();
-
-        return $model::find($id, $throw);
-    }
-
-    /**
-     * @param array $params
-     *
-     * @return QB
-     */
-    public function ransack(array $params)
-    {
-        $model = $this->modelName();
-
-        return $model::ransack($params);
-    }
-
-    /**
-     * @param array $params
-     *
-     * @return QB
-     */
-    public function ransackForUpdate(array $params)
-    {
-        $model = $this->modelName();
-
-        return $model::ransackForUpdate($params);
-    }
-
-    public function exists(array $ransack_params): bool
-    {
-        $model = $this->modelName();
-
-        return $model::exists($ransack_params);
     }
 
     /**
@@ -126,5 +127,12 @@ abstract class AbstractRepository extends AbstractSingleton implements Repositor
     public function paginate(array $params, int $page = 1, array $as_json_includes = [], int $per_page = null): PaginatedVO
     {
         return $this->ransack($params)->paginate($page, $as_json_includes, $per_page);
+    }
+
+    public function __call($method, $args)
+    {
+        $model = $this->modelName();
+
+        return call_user_func_array("$model::$method", $args);
     }
 }
