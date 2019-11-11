c52c25b487b86e8574f0247e2d806c43d5910899
Marcos Paliari <marcos@paliari.com.br>
2019-11-11 15:33:53 -0300

    fix RepositoryInterface

--- src/Db/RepositoryInterface.php
+++ src/Db/RepositoryInterface.php
@@ -6,7 +6,6 @@ use Paliari\PhpSetup\Db\VO\PaginatedVO;
 
 interface RepositoryInterface
 {
-
     public function create(array $params, bool $throw = false);
 
     public function update($id, array $params, bool $throw = true): bool;
@@ -15,22 +14,5 @@ interface RepositoryInterface
 
     public function delete($id, bool $throw = true): bool;
 
-    public function find($id, bool $throw = false);
-
-    /**
-     * @param array $params
-     *
-     * @return QB
-     */
-    public function ransack(array $params);
-
-    /**
-     * @param array $params
-     *
-     * @return QB
-     */
-    public function ransackForUpdate(array $params);
-
     public function paginate(array $params, int $page = 1, array $as_json_includes = [], int $per_page = null): PaginatedVO;
-
 }
